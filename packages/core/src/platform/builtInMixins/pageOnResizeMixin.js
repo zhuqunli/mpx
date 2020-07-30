9c605c33cd280ea1362c616e493bcd3397775b2c
xuegan <17801031786@163.com>
2020-07-30 21:47:53 +0800

    :fix: fix lint error

--- packages/core/src/platform/builtInMixins/pageOnResizeMixin.js
+++ packages/core/src/platform/builtInMixins/pageOnResizeMixin.js
@@ -1,14 +1,14 @@
-// web 页面尺寸发生改变的时间
+
 export default function onResize (mixinType) {
   if (mixinType === 'page' && __mpx_mode__ === 'web') {
     return {
       activated () {
         if (this.onResize) {
           const onResizeMethod = this.onResize
-          window.onresize = function() {
+          window.onresize = function () {
             var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
             var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
-            onResizeMethod({size: {windowWidth: w, windowHeight: h}})
+            onResizeMethod({ size: { windowWidth: w, windowHeight: h } })
           }
         }
       }
